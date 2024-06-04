5a827645fb3a790bfdb973bf6e4eccd0cdd8d9b0
Brandon Tapiba <brandontapiba@gmail.com>
2024-06-04 17:16:36 +0100

    working on the front end designs

--- public/JS/functions.js
+++ public/JS/functions.js
@@ -1,16 +1,27 @@
-$(document).ready(function() {
-    // Get the current page filename (without extension)
-    var currentPage = window.location.pathname.split('/').pop().split('.')[0];
-  
-    // Remove any existing active class
-    $(".nav-link").removeClass("active");
-  
-    // Find the navigation link matching the current page and add active class
-    $(".nav-link").each(function() {
-      var linkHref = $(this).attr("href").split('/').pop().split('.')[0];
-      if (currentPage === linkHref) {
-        $(this).addClass("active");
-      }
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  var url = window.location.href;
+  var links = document.querySelectorAll('.navbar .nav-link');
+
+  // Remove 'active' class from all links except the home page link
+  links.forEach(function(link) {
+    if (link.getAttribute('href') !== '/') {
+      link.classList.remove('active');
+    }
   });
-  
+
+  // Find the link whose URL matches the current URL and add 'active' class to it
+  links.forEach(function(link) {
+    var linkUrl = link.href;
+    if (url.indexOf(linkUrl) !== -1 ) {
+      link.classList.add('active');
+    }
+  });
+
+  // Special handling for the home page link
+  var homeLink = document.querySelector('.navbar .nav-link[href="/"]');
+  if (url.endsWith('/') || url.endsWith('#')) {
+    homeLink.classList.add('active');
+  } else {
+    homeLink.classList.remove('active');
+  }
+});
